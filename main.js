4f14fef8c87b0f05b43ba40a81ce0465d9f038e4
Gökhan SUÇSUZ <coolpisces22@gmail.com>
2024-01-07 20:22:41 +0300

    finished

---
+++ main.js
@@ -0,0 +1,45 @@
+//doviz.dev/v1/usd.json
+
+const currency = ["usd", "eur", "aud", "gbp", "jpy", "chf"];
+
+setInterval(() => {
+  fetch("https://doviz.dev/v1/try.json")
+    .then((res) => res.json())
+    .then((getCurrency) => {
+      for (let i = 0; i < currency.length; i++) {
+        document.querySelectorAll(".h4").forEach((item) => {
+          item.remove();
+        });
+      }
+
+      for (let i = 0; i < currency.length; i++) {
+        let text = getCurrency[`${currency[i].toUpperCase()}TRY`];
+        if (parseFloat(currency[i].innerHTML) < parseFloat(text)) {
+          const tagValue = document.querySelector(`.${currency[i]}`);
+          tagValue.parentElement.innerHTML = `
+            <i class="bi bi-arrow-down-left text-danger fw-bolder fs-5"></i><h4 class="text-danger px-2">${Number(
+              text
+            ).toFixed(4)}</h4>
+            `;
+        } else if (parseFloat(currency[i].innerHTML) < parseFloat(text)) {
+          const tagValue = document.querySelector(`.${currency[i]}`);
+          if (tagValue != null) {
+            tagValue.parentElement.innerHTML = `
+            <i class="bi bi-arrow-up-right text-success fw-bolder fs-5"></i><h4 class="text-success px-2">${Number(
+              text
+            ).toFixed(4)}</h4>
+            `;
+          }
+        } else {
+          const tagValue = document.querySelector(`.${currency[i]}`);
+          if (tagValue != null) {
+            tagValue.parentElement.innerHTML = `
+            <i class="bi bi-arrow-left-right text-secondary fw-bolder fs-5"></i><h4 class="text-secondary px-2">${Number(
+              text
+            ).toFixed(4)}</h4>
+            `;
+          }
+        }
+      }
+    });
+}, 500);
