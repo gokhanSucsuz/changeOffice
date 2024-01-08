d171bd8b31fe4ab7e63d24f6adb3b01ec763804c
Gökhan SUÇSUZ <coolpisces22@gmail.com>
2024-01-08 12:11:29 +0300

    changes

--- main.js
+++ main.js
@@ -1,14 +1,42 @@
 //doviz.dev/v1/usd.json
-
+let usd,
+  eur,
+  aud,
+  gbp,
+  jpy,
+  chf = 0;
 const currency = ["usd", "eur", "aud", "gbp", "jpy", "chf"];
+const currencyObject = [
+  {
+    usd: "0",
+    eur: "0",
+    aud: "0",
+    gbp: "0",
+    jpy: "0",
+    chf: "0"
+  }
+];
+setInterval(() => {
+  fetch("https://doviz.dev/v1/try.json")
+    .then((res) => res.json())
+    .then((getCurrency) => {
+      for (let i = 0; i < currency.length; i++) {
+        currencyObject.usd = getCurrency[`USDTRY`];
+        currencyObject.eur = getCurrency[`EURTRY`];
+        currencyObject.aud = getCurrency[`AUDTRY`];
+        currencyObject.gbp = getCurrency[`GBPTRY`];
+        currencyObject.jpy = getCurrency[`JPYTRY`];
+        currencyObject.chf = getCurrency[`CHFTRY`];
+      }
+      console.log(currencyObject);
+    });
+}, 5000);
 
 setInterval(() => {
   fetch("https://doviz.dev/v1/try.json")
     .then((res) => res.json())
     .then((getCurrency) => {
-      let currencyDiv;
       for (let i = 0; i < currency.length; i++) {
-        currencyDiv = document.querySelector(`.${currency[i]}`);
         document.querySelectorAll(".h4").forEach((item) => {
           item.remove();
         });
@@ -17,14 +45,16 @@ setInterval(() => {
       for (let i = 0; i < currency.length; i++) {
         let text = getCurrency[`${currency[i].toUpperCase()}TRY`];
 
-        if (parseFloat(currencyDiv.innerHTML) < parseFloat(text)) {
+        let value = currency[i];
+        console.log(currencyObject.usd);
+        if (parseFloat(currencyObject.value) < parseFloat(text)) {
           const tagValue = document.querySelector(`.${currency[i]}`);
           tagValue.parentElement.innerHTML = `
             <i class="bi bi-arrow-down-left text-danger fw-bolder fs-5"></i><h4 class="text-danger px-2">${Number(
               text
             ).toFixed(4)}</h4>
             `;
-        } else if (parseFloat(currency[i].innerHTML) < parseFloat(text)) {
+        } else if (parseFloat(currencyObject.usd) < parseFloat(text)) {
           const tagValue = document.querySelector(`.${currency[i]}`);
           if (tagValue != null) {
             tagValue.parentElement.innerHTML = `
@@ -45,4 +75,4 @@ setInterval(() => {
         }
       }
     });
-}, 3000);
+}, 300);
